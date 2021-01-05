9ef8c012eb209ec642ad905554304c3f2cea8af2
Somelina Obiechina <ifeasome@gmail.com>
2021-01-04 21:58:03 -0600

    initial set up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Fish Burger');
+INSERT INTO burgers (burger_name) VALUES ('Ramen Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pork Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Stewed Taco Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Leg Cheese Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Clam Bake Burger');
