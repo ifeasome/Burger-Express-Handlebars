f1b2b36d017431e790a0bd691fdddd616b641692
Somelina Obiechina <ifeasome@gmail.com>
2021-01-08 22:30:41 -0600

    Update burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,18 +16,18 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burger", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   burger.create([
     "name", "devoured"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burger/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   let condition = "id = " + req.params.id;
 
   console.log("condition", condition);
