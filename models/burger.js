ad2de5aed4ee6467b8d3682bfefeeb6f792a0afe
Somelina Obiechina <ifeasome@gmail.com>
2021-01-08 22:30:53 -0600

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -3,18 +3,18 @@ let orm = require("../config/orm.js");
 
 let burger = {
   all: function(cb) {
-    orm.all("burger", function(res) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("burger", cols, vals, function(res) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("burger", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
