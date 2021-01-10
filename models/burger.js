652789346bb097c13b3cd96ccc85e14bad02d728
Somelina Obiechina <ifeasome@gmail.com>
2021-01-10 00:57:41 -0600

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -8,13 +8,18 @@ let burger = {
     });
   },
   // The variables cols and vals are arrays.
-  insert: function(cols, vals, cb) {
-    orm.insert("burgers", cols, vals, function(res) {
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("burgers.id", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
